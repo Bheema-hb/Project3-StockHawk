34b9b7d095235f27d7055dbb70a67dd27f251513
Bheema <bheemaray@techjini.com>
2016-12-20 18:16:33 +0530

    Initial commit

---
+++ StockHawkNew/StockHawk-Bheema/app/build/generated/source/dataBinding/androidTest/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="66a0ee9f-512b-4e92-80a7-02db234af672", modulePackage="com.udacity.stockhawk.test", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Android-Projects/NanoDegree/StockHawk-Bheema/app/build/intermediates/data-binding-info/androidTest/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
---
+++ StockHawkNew/StockHawk-Bheema/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="0727826f-9820-4d28-9946-c0b329589de2", modulePackage="com.udacity.stockhawk", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Android-Projects/NanoDegree/StockHawk-Bheema/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
